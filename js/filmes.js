e7e14ec701ea7ad46913eb8def3d53e653f8271a
PIETRA PAULA VOLPATO <23111825@CFP127ALUNO.LOCAL>
2024-02-29 17:14:45 -0300

    nao-concluido

---
+++ js/filmes.js
@@ -0,0 +1,17 @@
+//responsavel por interagir com os filmes
+
+export async function getFilmes (){
+    const url = 'http://localhost:8080/v2/acmeFilmes/filmes'
+    const response = await fetch(url)
+    const data= await response.json()
+
+    return data.filmes
+}
+
+export async function getFilme (id) {
+    const url = `http://localhost:8080/v2/acmefilmes/filme/${id}`
+    const response = await fetch(url)
+    const data= await response.json()
+
+    return data.filme[0]
+}
